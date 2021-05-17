60b08a8ad5f7780a5995dfd0277ae97027bc7ee4
kakamilala <78193801+kakamilala@users.noreply.github.com>
2021-05-17 19:10:13 +0300

    Update and rename index.php to mail.php

---
+++ mail.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>MebelPlusKzn</title>
+
+    
+</head>
+ <?php
+$to = "kamilaibr2002@gmail.com"; // емайл получателя данных из формы
+$tema = "Форма обратной связи на PHP"; // тема полученного емайла
+$message = "Ваше имя: ".$_POST['lastname']."<br>";//присвоить переменной значение, полученное из формы name=name
+  $message .= "E-mail: ".$_POST['email']."<br>"; //полученное из формы name=email
+$message .= "Номер телефона: ".$_POST['number']."<br>"; //полученное из формы name=phone
+$headers  = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
+  $headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
+mail($to, $tema, $message, $headers); //отправляет получателю на емайл значения переменных
+?>
+     
+</body>
+</html>
